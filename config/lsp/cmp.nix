d22951cac520a2c3e740aa26b43a669733996e45
argonarch <armando12123434@gmail.com>
2025-02-10 06:38:01 -0300

    style: cmp order

---
+++ config/lsp/cmp.nix
@@ -0,0 +1,50 @@
+{
+  plugins = {
+    cmp-buffer.enable = true;
+    cmp-path.enable = true;
+    cmp-dap.enable = true;
+    cmp-nvim-lsp.enable = true;
+    cmp_luasnip.enable = true;
+    # cmp-vsnip.enable = true;
+
+    cmp = {
+      enable = true;
+      autoEnableSources = true;
+      settings = {
+        sources = [
+          {name = "path";}
+          {name = "nvim_lsp";}
+          {name = "luasnip";}
+          {name = "buffer";}
+          {name = "dap";}
+          {name = "neorg";}
+          # {name = "vsnip";}
+        ];
+        mapping = {
+          "<C-Space>" = "cmp.mapping.complete()";
+          "<C-d>" = "cmp.mapping.scroll_docs(-4)";
+          "<C-e>" = "cmp.mapping.close()";
+          "<C-f>" = "cmp.mapping.scroll_docs(4)";
+          "<CR>" = "cmp.mapping.confirm({ select = true })";
+          "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+          "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+          "<S-CR>" = "cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true })";
+        };
+        snippet = {
+          expand = "function(args) require('luasnip').lsp_expand(args.body) end";
+        };
+        formatting = {
+          fields = ["kind" "abbr" "menu"];
+          expandableIndicator = true;
+        };
+        window = {
+          completion = {
+            border = "rounded";
+            winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:Visual,Search:None";
+          };
+          documentation = {border = "rounded";};
+        };
+      };
+    };
+  };
+}
